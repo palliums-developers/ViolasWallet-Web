8fcce736bcb706d3fe66e7430588552aa68fa64f
nissaka <maigechuang115@163.com>
2019-12-26 15:13:35 +0800

    add sh function and change port

---
+++ sh/init.sh
@@ -0,0 +1,4 @@
+npm install
+cd ../src/utils/
+node currencyToken.js
+cp -rf ./kulap-libra-violas ../../node_modules/
---
+++ sh/start.sh
@@ -0,0 +1,2 @@
+nohup node ../exchange_forwarding_api/index.js > exchange.log 2>&1 &
+nohup npm start > nohup.out 2>&1 &
