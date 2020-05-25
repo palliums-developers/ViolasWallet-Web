e69920f317f6759bb96b52b21504af019374ee68
nissaka <maigechuang115@163.com>
2020-05-25 16:17:10 +0800

    fixed tyArgs and code

--- src/App.js
+++ src/App.js
@@ -82,9 +82,9 @@ class App extends React.Component {
     const tx = {
       from: this.state.from,
       payload: {
-        code: this.state.code,
+        code: 'a11ceb0b010007014600000004000000034a0000000c000000045600000002000000055800000009000000076100000029000000068a00000010000000099a0000001200000000000001010200010101000300010101000203050a020300010900063c53454c463e0c4c696272614163636f756e740f7061795f66726f6d5f73656e646572046d61696e00000000000000000000000000000000010000ffff030005000a000b010a023e0002',
         tyArgs: [
-          this.state.tyArgs
+          '0600000000000000000000000000000000034c4252015400'
         ],
         args: [
           {
@@ -177,8 +177,8 @@ class App extends React.Component {
           <button onClick={this.getAccount}>get accounts</button>
           <div className='tx'>
             <p>From: <input type="text" onChange={this.handelChange.bind(this, 'from')} /></p>
-            <p>Code: <input type="text" onChange={this.handelChange.bind(this, 'code')} /></p>
-            <p>tyArgs: <input type="text" onChange={this.handelChange.bind(this, 'tyArgs')} /></p>
+            {/* <p>Code: <input type="text" onChange={this.handelChange.bind(this, 'code')} /></p> */}
+            {/* <p>tyArgs: <input type="text" onChange={this.handelChange.bind(this, 'tyArgs')} /></p> */}
             <p>Address: <input type="text" onChange={this.handelChange.bind(this, 'address')} /></p>
             <p>Value: <input type="text" onChange={this.handelChange.bind(this, 'value')} /></p>
           </div>
