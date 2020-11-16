e02f7d6458b1c671d63d1fd37920f7bb63cf1fc4
nissaka <maigechuang115@163.com>
2020-11-16 15:33:13 +0800

    modify operation.sh

--- operation.sh
+++ operation.sh
@@ -1,3 +1,6 @@
 git pull origin newnet_web
+echo '*****finished pull and start building'
 yarn build
+echo '*****finished build and start copy'
 sudo cp -rf ./build/* /var/www/newnet.violas.io/
+echo '*****All finished'
