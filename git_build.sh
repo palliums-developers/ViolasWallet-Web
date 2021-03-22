007dc8420dadb00de14af67350b9f1b5b12a4045
nissaka <maigechuang115@163.com>
2021-03-22 10:06:37 +0800

    git_build.sh problem

--- git_build.sh
+++ git_build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
-git pull origin inside
-=======
 git pull origin outside
->>>>>>> 4a3c8b6f7142b8f77b53b27147b47e5687896e9a
 yarn build
 sudo cp -rf ./build/* /var/www/devwallet.violas.io/
 date
